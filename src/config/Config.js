a45b4726cc5866a2f8b65ed6d7615c265eaf2f3d
Shaney265 <124664324+Shaney265@users.noreply.github.com>
2023-06-14 04:02:51 +1000

    Update Config.js

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBRLU8My3cFx7Q9j05RlpGcADE2jSpHKvs",
-    authDomain: "classproject-937e8.firebaseapp.com",
-    projectId: "classproject-937e8",
-    storageBucket: "classproject-937e8.appspot.com",
-    messagingSenderId: "669600973558",
-    appId: "1:669600973558:web:72e86eca25137e80d622d7"
+  apiKey: "AIzaSyBTeJlkJ_wS3HmqGKzHclr-1SVkasFZdaY",
+  authDomain: "movie-final-project.firebaseapp.com",
+  projectId: "movie-final-project",
+  storageBucket: "movie-final-project.appspot.com",
+  messagingSenderId: "795236576994",
+  appId: "1:795236576994:web:422918708d9b19775eb8bb"
 };
   
