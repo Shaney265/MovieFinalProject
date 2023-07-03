2c3d7c8919ea06c0e56734dc72e3a6849caa3df2
shandel <shandelskulsky@gmail.com>
2023-07-04 03:54:41 +1000

    Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import '../styles/About.css'
+import '../styles/Contact.css'
 
 import { useContext, useEffect, useState } from 'react';
 import { collection, getDocs } from "firebase/firestore";
@@ -11,49 +11,66 @@ import { ref, getDownloadURL, getStorage } from "firebase/storage";
 import { FBDbContext } from '../contexts/FBDbContext';
 import { FBStorageContext } from '../contexts/FBStorageContext';
 
-export function Contact () {
+export function Contact() {
 
-const storage = getStorage();
+    const storage = getStorage();
 
 
 
-getDownloadURL(ref(storage, 'movie_cover/Contactus3.jpg'))
-  .then((url) => {
-    const img = document.getElementById('myimg');
-    img.setAttribute('src', url);
-    if (img) {
-        img.setAttribute('src', url)
-        } 
-        else {
-          console.log('img is null')
-      }
-  })
+    getDownloadURL(ref(storage, 'movie_cover/Contactus3.jpg'))
+        .then((url) => {
+            const img = document.getElementById('myimg');
+            img.setAttribute('src', url);
+            if (img) {
+                img.setAttribute('src', url)
+            }
+            else {
+                console.log('img is null')
+            }
+        })
 
 
 
     return (
         <Container >
-       
-            <Col >
-            <h1></h1>
+            <Row>
 
-            <div >
-                <center><h1>
-                    </h1> <img id="myimg" src="" alt="Contactus3" /> <h1><p></p></h1>
-                    <h1> Welcome to Animation Movie Madness!!</h1>
-                    <h1><p></p></h1>
-                    <h1></h1>
-                    <h3> Classics, Modern and Niche Titles</h3>
                 
-                <h5><p>A place to browse, search or reaquaint yourself with some wonderful animated movies</p> <p> Feel free to browse and read through all the existing reviews or sign up to submit your own.</p></h5>
-                </center>
-            
-            
-            </div>
+                    <h1></h1>
 
- 
-           
-            </Col>
+                    <div >
+                        <center>
+                        <img id="myimg" src="" alt="Contactus3" />
+                        </center>
+                        <h1></h1>
+
+                    </div>
+                
+            </Row>
+            <Row>
+                <Col  > 
+                <center class ="borderdesign">
+                <h1 class= "headingcolor" >Head Office</h1>
+                <p >23 Holiday Dr, Newport NSW 2686</p>
+                <p>contactus@amm.com.au</p>
+                <p>02 9569 4585</p>
+                </center>
+                </Col>
+                <Col  > 
+                <center class ="borderdesign">
+                <h1 class= "headingcolor">Help Desk</h1>
+                <p class = "textcolor" > Issues regarding your account.</p>
+                <p> Contact 1800 6558 212</p>
+                <p>Between 9am - 6pm</p>
+                </center>
+                </Col>
+{/*                 <Col > 
+                <center>
+                <h1>Advertising</h1>
+                <p>This is another paragraph.</p>
+                </center>
+                </Col> */}
+            </Row>
         </Container>
     )
 }
